d7fa9b7aab384d08317527d54c5b37ac1c2eab7d
LAPTOP-KCQO4K14\黄海 <2816459729@qq.com>
2022-03-14 16:58:33 +0800

    2022/3/14/16:59hello.c

---
+++ hello.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	int a[5]={2,4,6,8,9};
+	int x,i,flag=1;
+	printf("请输入一个整数：");
+	scanf("%d",&x);
+	for(i=0;i<5;i++)
+	{
+		if(x==a[i])
+		{
+			printf("index:%d",i);
+			flag=0;
+		}
+	}
+	 	if(flag==1)
+	 	printf("index:-1");
+}
